496ed966f7a4a6b92cb79a64c4362b3b9b1d8143
BethelHills <bethelhillary@gmail.com>
2025-11-07 13:23:34 -0800

    Fix UserProfile.jsx import path to use correct UserContext location

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import UserContext from '../context/UserContext';
+import UserContext from '../UserContext';
 
 const UserProfile = (props) => {
   const contextUser = useContext(UserContext);
