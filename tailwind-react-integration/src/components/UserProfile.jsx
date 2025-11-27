e7e0a5ef067afb48b1ba15895c282cdbf6bfedc2
BethelHills <bethelhillary@gmail.com>
2025-11-26 23:11:27 -0800

    feat: add hover scale and transition to profile image

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,7 +5,7 @@ function UserProfile() {
   <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <div className="md:flex items-center">
         <img
-          className="rounded-full sm:w-24 sm:h-24 w-24 h-24 md:w-36 md:h-36 mx-auto md:mx-0 md:mr-6 object-cover border-2 border-indigo-500"
+          className="rounded-full sm:w-24 sm:h-24 w-24 h-24 md:w-36 md:h-36 mx-auto md:mx-0 md:mr-6 object-cover border-2 border-indigo-500 transform hover:scale-110 transition-transform duration-300 ease-in-out"
           src="https://via.placeholder.com/150"
           alt="User"
         />
