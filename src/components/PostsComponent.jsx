b3f9a08368052b1292edbedc5ccc2f51b3abeab4
BethelHills <bethelhillary@gmail.com>
2025-12-17 02:29:58 -0800

    fix: add main.jsx and align PostsComponent with @tanstack/react-query

--- src/components/PostsComponent.jsx
+++ src/components/PostsComponent.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 
 const fetchPosts = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
